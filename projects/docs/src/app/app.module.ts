190f075662e1b1c7004f3df3ae98a9d2f43d7fa6
TryHardDood <10364896+TryHardDood@users.noreply.github.com>
2020-12-04 15:31:17 +0100

    Small update

--- projects/docs/src/app/app.module.ts
+++ projects/docs/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { MenuModule, TransitionModule } from 'ngx-headlessui';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenuModule } from 'projects/ngx-headlessui/src/components/menu/menu.module';
 import { NgModule } from '@angular/core';
 import { NgxMenuPageComponent } from './pages/ngx-menu-page/ngx-menu-page.component';
-import { PreviewComponent } from './components/preview/preview.component';
 import { NgxTransitionPageComponent } from './pages/ngx-transition-page/ngx-transition-page.component';
+import { PreviewComponent } from './components/preview/preview.component';
+import { TransitionModule } from 'projects/ngx-headlessui/src/components/transition/transition.module';
 
 @NgModule({
   declarations: [
--- projects/ngx-headlessui/src/components/menu/menu.component.ts
+++ projects/ngx-headlessui/src/components/menu/menu.component.ts
@@ -1,8 +1,6 @@
 import { AfterViewInit, Component, ElementRef, HostListener, Input, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
-import { AnimationBuilder, AnimationMetadata, animate, style } from '@angular/animations';
 
 import { fromEvent } from 'rxjs';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-menu',
@@ -26,9 +24,6 @@ export class MenuComponent implements AfterViewInit {
   @ViewChild('menuButtonContainer', { read: ViewContainerRef })
   menuButtonContainer!: ViewContainerRef | any;
 
-  @ViewChild('menuItemsContainer', { read: ViewContainerRef })
-  menuItemsContainer!: ViewContainerRef | any;
-
   @Input()
   closeOnItemClick: boolean = true;
 
@@ -38,14 +33,7 @@ export class MenuComponent implements AfterViewInit {
   @Input()
   disableAnimations: boolean = false;
 
-  private openAnimation: any;
-  private closeAnimation: any;
-
-  menuElementRef: ElementRef | any;
-
-  constructor(private elementRef: ElementRef, private animationBuilder: AnimationBuilder) {
-    this.openAnimation = this.animationBuilder.build(this.getOpenAnimation());
-    this.closeAnimation = this.animationBuilder.build(this.getCloseAnimation());
+  constructor(private elementRef: ElementRef) {
   }
 
   ngAfterViewInit(): void {
@@ -53,9 +41,7 @@ export class MenuComponent implements AfterViewInit {
     this.menuButtonContainer.insert(view);
 
     const button = this.menuButtonContainer.get(0)?.rootNodes[0];
-
     fromEvent(button, 'click').subscribe(() => {
-      // Closing with the trigger button
       if (this.opened) {
         this.closeMenu();
         return;
@@ -73,58 +59,20 @@ export class MenuComponent implements AfterViewInit {
     }
   }
 
-  private getOpenAnimation(): AnimationMetadata[] {
-    return [
-      style({ opacity: 0, transform: 'scaleX(.95) scaleY(.95)' }),
-      animate('100ms ease-out', style({ opacity: 1, transform: 'scaleX(1) scaleY(1)' }))
-    ];
-  }
-
-  private getCloseAnimation(): AnimationMetadata[] {
-    return [
-      style({ opacity: 1, transform: 'scaleX(1) scaleY(1)' }),
-      animate('75ms ease-in', style({ opacity: 0, transform: 'scaleX(.95) scaleY(.95)' }))
-    ];
-  }
-
   private openMenu(): void {
     this.opened = true;
-    setTimeout(() => {
-      this.menuElementRef = this.menuItemsContainer.get(0)?.rootNodes[0];
+    /*fromEvent(this.transitionComponent.nativeElement, 'click').pipe(take(1)).subscribe((event: any) => {
+      if (event.target && event.target.getAttribute('disabled') !== null) {
+        return;
+      }
 
-      this.playAnimation(true);
-
-      fromEvent(this.menuElementRef, 'click').pipe(take(1)).subscribe((event: any) => {
-        if (event.target && event.target.getAttribute('disabled') !== null) {
-          return;
-        }
-
-        if (this.closeOnItemClick) {
-          this.closeMenu();
-        }
-      });
-    }, 0);
+      if (this.closeOnItemClick) {
+        this.closeMenu();
+      }
+    });*/
   }
 
   private closeMenu(): void {
-    this.playAnimation(false);
-    setTimeout(() => {
-      this.opened = false;
-      this.menuElementRef = undefined;
-    }, 100);
-  }
-
-  private playAnimation(opened: boolean): void {
-    if (this.disableAnimations || !this.menuElementRef) {
-      return;
-    }
-
-    let player;
-    if (opened) {
-      player = this.openAnimation.create(this.menuElementRef);
-    } else {
-      player = this.closeAnimation.create(this.menuElementRef);
-    }
-    player.play();
+    this.opened = false;
   }
 }
