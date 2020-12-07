63565bb1ae29232141368463e57bd98fcb2fd02a
TryHardDood <rsnconfigs@gmail.com>
2020-12-07 08:29:44 +0100

    #8: Added syntax highlighting

--- projects/docs/src/app/app.module.ts
+++ projects/docs/src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { IndexComponent } from './pages/index/index.component';
 import { MenuModule } from 'projects/ngx-headlessui/src/components/menu/menu.module';
 import { NgModule } from '@angular/core';
 import { NgxMenuPageComponent } from './pages/ngx-menu-page/ngx-menu-page.component';
 import { NgxTransitionPageComponent } from './pages/ngx-transition-page/ngx-transition-page.component';
 import { PreviewComponent } from './components/preview/preview.component';
 import { TransitionModule } from 'projects/ngx-headlessui/src/components/transition/transition.module';
-import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
@@ -22,10 +24,19 @@ import { IndexComponent } from './pages/index/index.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HighlightModule,
+
     MenuModule,
     TransitionModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
