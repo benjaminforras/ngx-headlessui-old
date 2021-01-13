9f549b39b4c4c63fe5b9f7d386e75e6f5f7f5dd5
TryHardDood <10364896+TryHardDood@users.noreply.github.com>
2021-01-13 10:33:09 +0100

    perf(ngx-transition): Rewritten transition component

--- projects/ngx-headlessui/src/components/transition/transition.component.ts
+++ projects/ngx-headlessui/src/components/transition/transition.component.ts
@@ -1,32 +1,45 @@
 import {
-  AfterContentInit,
   Component,
+  ContentChildren,
   EventEmitter,
   HostBinding,
   Input,
-  OnChanges,
+  OnInit,
   Output,
-  SimpleChanges,
+  QueryList,
   ViewChild,
   ViewContainerRef
 } from '@angular/core';
 import { Reason, transition } from './utils/transitions';
+import { first, skip } from 'rxjs/operators';
+
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: '[ngx-transition], transition, ngx-transition',
+  selector: '[ngx-transition], [transition], transition, ngx-transition',
   templateUrl: './transition.component.html',
-  styles: [],
 })
-export class TransitionComponent implements OnChanges, AfterContentInit {
+export class TransitionComponent implements OnInit {
 
-  @ViewChild('transitionContainer', { read: ViewContainerRef })
-  transitionContainer!: ViewContainerRef | any;
+  @ContentChildren(TransitionComponent, { descendants: true })
+  _children: QueryList<TransitionComponent> = new QueryList<TransitionComponent>();
+
+  @ViewChild('transitionContainer', { read: ViewContainerRef, static: true })
+  _viewContainer!: ViewContainerRef | any;
 
   /**
    * Whether to show or not the contents of ngx-transition
    */
   @Input()
-  show: boolean = false;
+  set show(value: boolean) {
+    this._show.next(value);
+  }
+
+  get show(): boolean {
+    return this._show.value;
+  }
+
+  _show: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   /**
    * Classes to be applied on the element when showing
@@ -60,65 +73,100 @@ export class TransitionComponent implements OnChanges, AfterContentInit {
   @Output()
   afterLeave: EventEmitter<any> = new EventEmitter();
 
-  init: boolean = true;
   closed: boolean = true;
-  private elementRef!: HTMLElement;
+
+  private _elementRef!: HTMLElement;
+  private _init: boolean = true;
 
   constructor() {
+  }
+
+  @HostBinding('style.display')
+  get getDisplay(): string {
+    return this._init ? 'none' : '';
+  }
+
+  ngOnInit(): void {
     this.closed = !this.show;
-  }
 
-  @HostBinding('style.visibility')
-  get getVisibility(): string {
-    return this.init ? 'hidden' : 'visible';
-  }
-
-  ngAfterContentInit(): void {
-    this.init = !this.show;
     if (this.closed) {
-      this.closed = false;
-      setTimeout(() => {
-        this.elementRef = this.transitionContainer.get(0).rootNodes.find((element: HTMLElement) => element.tagName);
-
+      this._init = true;
+      if (this.closed) {
+        this.closed = false;
         setTimeout(() => {
+          this._elementRef = this._getElement();
           this.closed = true;
-          this.init = false;
         });
+      }
+    } else {
+      setTimeout(() => {
+        this._elementRef = this._getElement();
+        this._updateView();
       });
     }
+
+    this._show.pipe(skip(1)).subscribe(() => {
+      this._updateView();
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.show) {
-      if (this.show) {
-        this.closed = false;
-        this.beforeEnter.emit();
-        this.transitionEnter(this.elementRef);
-      } else {
-        if (!this.transitionContainer) {
-          return;
+  private async _updateView(): Promise<any> {
+    if (this.show) {
+      this.closed = false;
+      this._transitionEnter();
+    } else {
+      if (this._children.length > 0) {
+        let childrenFinished: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+        for (let child of this._children.toArray().reverse()) {
+          child.afterLeave.pipe(first()).subscribe(() => {
+            childrenFinished.pipe(first()).subscribe((value) => {
+              childrenFinished.next(++value);
+            });
+          });
+          child.show = false;
         }
-        this.elementRef = this.transitionContainer.get(0).rootNodes.find((element: HTMLElement) => element.tagName);
-        this.beforeLeave.emit();
-        this.transitionLeave(this.elementRef);
+
+        childrenFinished.subscribe((value) => {
+          if (value === this._children.length)
+            this._transitionLeave();
+        });
+      } else {
+        this._transitionLeave();
       }
     }
   }
 
-  private transitionEnter(elementRef: HTMLElement): void {
-    transition(elementRef, this.useSplitClasses(this.enter), this.useSplitClasses(this.enterFrom), this.useSplitClasses(this.enterTo), (reason: Reason) => {
+  private _getElement(): HTMLElement {
+    if (!this._elementRef) {
+      this._elementRef = this._viewContainer.get(0).rootNodes.find((element: HTMLElement) => element.tagName);
+      this._init = false;
+    }
+    return this._elementRef;
+  }
+
+  private _transitionEnter(): void {
+    this.beforeEnter.emit();
+    transition(this._getElement(), this._useSplitClasses(this.enter), this._useSplitClasses(this.enterFrom), this._useSplitClasses(this.enterTo), (reason: Reason) => {
       this.afterEnter.emit();
+
+      if (this._children.length > 0) {
+        this._children.forEach((child: TransitionComponent) => {
+          child.show = true;
+        });
+      }
     });
   }
 
-  private transitionLeave(elementRef: HTMLElement): void {
-    transition(elementRef, this.useSplitClasses(this.leave), this.useSplitClasses(this.leaveFrom), this.useSplitClasses(this.leaveTo), (reason: Reason) => {
+  private _transitionLeave(): void {
+    this.beforeLeave.emit();
+    transition(this._getElement(), this._useSplitClasses(this.leave), this._useSplitClasses(this.leaveFrom), this._useSplitClasses(this.leaveTo), (reason: Reason) => {
       this.afterLeave.emit();
       this.closed = true;
     });
   }
 
-  private useSplitClasses(classes: string = ''): string[] {
+  private _useSplitClasses(classes: string = ''): string[] {
     return classes.split(' ').filter(className => className.trim().length > 1);
   }
 }
